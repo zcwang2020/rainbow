94c9545846dd0e2375e16af2f0f87395ebbd2b49
zcwang <zcwang@tmindtech.com>
2024-09-10 10:41:46 +0800

    init:初始化项目

---
+++ white/src/main/java/com/white/WhiteApplication.java
@@ -0,0 +1,16 @@
+package com.white;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zcwang
+ */
+@SpringBootApplication
+public class WhiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteApplication.class, args);
+    }
+
+}
---
+++ white/src/test/java/com/white/WhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.white;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
