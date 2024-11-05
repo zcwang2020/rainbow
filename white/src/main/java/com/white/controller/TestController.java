526c3325cb6db3328b2d7fb095f25ac1adf5ee3d
zcwang <wb.wangzhichao01@mesg.corp.netease.com>
2024-11-05 21:20:20 +0800

    add: 地图经纬度信息查询

---
+++ white/src/main/java/com/white/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.white.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: tmind
+ * @Date: 2024/11/5 21:19
+ * @Description:
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/health")
+    public String health(){
+        return "health";
+    }
+}
+
